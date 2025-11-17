7821aa2a3166d2362a1c4d62474c4622616bd9d4
Jatin kumar <jatink07624@gmail.com>
2025-11-17 18:59:53 +0000

    insert logo-white for light mode and logo-dark for dark mode

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,16 +7,16 @@ export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn('relative', className)}>
       <Image
-        src="/logo-dark.svg"
-        alt="AtLink Logo Dark"
+        src="/logo-white.svg"
+        alt="AtLink Logo Light"
         fill
         priority
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         className="block dark:hidden"
       />
       <Image
-        src="/logo-light.svg"
-        alt="AtLink Logo Light"
+        src="/logo-dark.svg"
+        alt="AtLink Logo Dark"
         fill
         priority
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
